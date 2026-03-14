a25ff8807592e606fa4d0c5e37a14bb81a6230e3
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2026-03-14 14:00:50 +0200

    Add: configuration

---
+++ src/test/java/com/dimitarrradev/dimitarrradev/photoapp/api/gateway/AppTests.java
@@ -0,0 +1,13 @@
+package com.dimitarrradev.dimitarrradev.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
